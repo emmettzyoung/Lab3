d573dd42ad27d43ded9e7b887962af74ef5d51c3
emmettzyoung <emmett.zyoung@gmail.com>
2026-02-27 18:09:37 -0500

    Multiple changes made
    - Altered all files heavily to present a map with a window with adjustable options for users to select and de-select certain layers

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Lab3
 
+# Welcome to Emmett Young's Lab 3 Repository
+
+### In this repository, you will find the following:
+- index.html: Responsible for loading in necessary packages and displaying the front end code correctly, with features presented here such as buttons and headers.
+- script.js: Responsible for the functionality of elements defined in the index.html file, including the map, the drop-down menu, the checkboxes, and the hover mechanics for neighbourhoods as well.
+- style.css: Responsible for the styling of the body, map, buttons, headers, and text, all to ensure user experience is appealing. 
+- Neighbourhoods_Clean: Houses GeoJSON files.
+- Neighbourhoods_Shapefiles: Houses rough work shapefiles
+
+### The intended use of this map:
+This map is intended for those who wish to ice skate, particularily for Valentines day. This tool will allow those celebrating
+to decide which rink is closest to them, and think of whether or not skating could be their Valentine's day activity.
+
+### Notes of ownership:
+This map and the code included should not be copied without the owner being mentioned. Please mention any code copied was written by emmettzyoung on GitHub. Additionally, use of my mapbox token for other projects is not allowed, and users trying to make use of MapBox's API must retrieve and use their own access token.
--- script.js
+++ script.js
@@ -1,52 +1,178 @@
-// Access token for Mapbox API
 mapboxgl.accessToken = 'pk.eyJ1IjoiZW1tZXR0eW91bmciLCJhIjoiY21rNGI3Y3Z4MDV3ZjNrcHk2MXFrYTlpeSJ9.nhnMjjZj_o1eyXtp_Y8Svw';
 
-// Initialize the map to a const variable so that it cannot be reassigned later on in the code
 const map = new mapboxgl.Map({
     container: 'my-map',
     style: 'mapbox://styles/emmettyoung/cmlh2534n006s01qv686sadry',
-    center: [-79.38, 43.65],
-    zoom: 12,
+    center: [-79.38, 43.73],
+    zoom: 10,
     maxBounds: [
         [-80.110931, 43.425996],
-        [-78.857117, 44.085612]],
+        [-78.857117, 44.085612]
+    ]
 });
 
-// Add geocoder control to the map with a bounding box that limits search results to Toronto
-map.addControl(
-    new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl,
-        region: 'Ontario',
-        placeholder: 'Search for a location in Toronto',
-        bbox: [-79.6393, 43.5810, -79.1158, 43.8554]
-    })
-);
-
-// Add zoom and rotation controls to the map
 map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.FullscreenControl());
 
-// Load the GeoJSON data for the ice rinks (both indoor and outdoor) and the neighbourhoods, and add them as 
-// sources and layers to the map
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    countries: "ca"
+});
+
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+document.getElementById('returnbutton').addEventListener('click', () => {
+    map.flyTo({
+        center: [-79.38, 43.65],
+        zoom: 10,
+        essential: true
+    });
+});
+
+
+
+const allLayers = [
+    'neighbourhoods-quantile',
+    'neighbourhoods-natural',
+    'neighbourhoods-equal',
+    'unsuitable-quantile',
+    'unsuitable-natural',
+    'unsuitable-equal'
+];
+
+function showOnlyLayer(selectedId) {
+    allLayers.forEach(layerId => {
+        map.setLayoutProperty(
+            layerId,
+            'visibility',
+            layerId === selectedId ? 'visible' : 'none'
+        );
+    });
+}
+
+
 map.on('load', () => {
-    // Add sources for the neighbourhoods and ice rinks, promoting the _id property to be used as a unique 
-    // identifier for each feature
     map.addSource('neighbourhoods', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/emmettzyoung/Lab3/main/Neighbourhoods_Clean/neighbourhoods_cleaned.geojson',
-        promoteId: '_id'
+        promoteId: '_id'  // Make sure every feature has a '_id' property in your GeoJSON
     });
-        // Add layers for the neighbourhoods and ice rinks, using the feature state to change the opacity 
-    // and size of the features when they are hovered over
+
     map.addLayer({
-        'id': 'neighbourhoods-fill',
+        'id': 'neighbourhoods-quantile',
         'type': 'fill',
         'source': 'neighbourhoods',
         'paint': {
-            'fill-color': '#FFC4C4',
-            'fill-opacity': 1
+            'fill-color':         [
+                'step', // STEP expression produces stepped results based on value pairs
+                ['get', 'Med_Inc'], // GET expression retrieves property value from 'population' data field
+                '#eff3ff', // Colour assigned to any values < first step
+                67000.01, '#bdd7e7', // Colours assigned to values >= each step
+                72000.01, '#6baed6',
+                77500.01, '#3182bd',
+                85000.01, '#08519c'
+            ],
+            'fill-opacity': 0.5,
+            'fill-outline-color': 'white'
         }
     });
+
+    map.addLayer({
+        'id': 'unsuitable-quantile',
+        'type': 'fill',
+        'source': 'neighbourhoods',
+        'paint': {
+            'fill-color':         [
+                'step', // STEP expression produces stepped results based on value pairs
+                ['get', 'Not_suitab'], // GET expression retrieves property value from 'population' data field
+                '#eff3ff', // Colour assigned to any values < first step
+                380.01, '#bdd7e7', // Colours assigned to values >= each step
+                595.01, '#6baed6',
+                980.01, '#3182bd',
+                1410.01, '#08519c'
+            ],
+            'fill-opacity': 0.5,
+            'fill-outline-color': 'white'
+        }
+    });
+
+    map.addLayer({
+        'id': 'neighbourhoods-equal_interval',
+        'type': 'fill',
+        'source': 'neighbourhoods',
+        'paint': {
+            'fill-color':         [
+                'step', // STEP expression produces stepped results based on value pairs
+                ['get', 'Med_Inc'], // GET expression retrieves property value from 'population' data field
+                '#eff3ff', // Colour assigned to any values < first step
+                74400.01, '#bdd7e7', // Colours assigned to values >= each step
+                96800.01, '#6baed6',
+                119200.01, '#3182bd',
+                141600.01, '#08519c'
+            ],
+            'fill-opacity': 0.5,
+            'fill-outline-color': 'white'
+        }
+    });
+
+    map.addLayer({
+        'id': 'unsuitable-equal_interval',
+        'type': 'fill',
+        'source': 'neighbourhoods',
+        'paint': {
+            'fill-color':         [
+                'step', // STEP expression produces stepped results based on value pairs
+                ['get', 'Not_suitab'], // GET expression retrieves property value from 'population' data field
+                '#eff3ff', // Colour assigned to any values < first step
+                650.01, '#bdd7e7', // Colours assigned to values >= each step
+                1245.01, '#6baed6',
+                1840.01, '#3182bd',
+                2435.01, '#08519c'
+            ],
+            'fill-opacity': 0.5,
+            'fill-outline-color': 'white'
+        }
+    });
+
+    map.addLayer({
+        'id': 'neighbourhoods-geometric_interval',
+        'type': 'fill',
+        'source': 'neighbourhoods',
+        'paint': {
+            'fill-color':         [
+                'step', // STEP expression produces stepped results based on value pairs
+                ['get', 'Med_Inc'], // GET expression retrieves property value from 'population' data field
+                '#eff3ff', // Colour assigned to any values < first step
+                65450.521, '#bdd7e7', // Colours assigned to values >= each step
+                72301.779, '#6baed6',
+                85752.299, '#3182bd',
+                112158.617, '#08519c'
+            ],
+            'fill-opacity': 0.5,
+            'fill-outline-color': 'white'
+        }
+    });
+
+    map.addLayer({
+        'id': 'unsuitable-geometric_interval',
+        'type': 'fill',
+        'source': 'neighbourhoods',
+        'paint': {
+            'fill-color':         [
+                'step', // STEP expression produces stepped results based on value pairs
+                ['get', 'Not_suitab'], // GET expression retrieves property value from 'population' data field
+                '#eff3ff', // Colour assigned to any values < first step
+                412.280, '#bdd7e7', // Colours assigned to values >= each step
+                594.266, '#6baed6',
+                951.546, '#3182bd',
+                1652.964, '#08519c'
+            ],
+            'fill-opacity': 0.5,
+            'fill-outline-color': 'white'
+        }
+    });
+
     map.addLayer({
         'id': 'neighbourhoods-outline',
         'type': 'line',
@@ -56,4 +182,21 @@ map.on('load', () => {
             'line-width': 1
         }
     });
+
+    showOnlyLayer('neighbourhoods-quantile');
+});
+
+// 3) Change map layer display based on check box using setLayoutProperty method
+document.getElementById('layercheck').addEventListener('change', (e) => {
+    const selectedLayer = document.getElementById('layer-select').value;
+    map.setLayoutProperty(
+        selectedLayer,
+        'visibility',
+        e.target.checked ? 'visible' : 'none'
+    );
+});
+
+document.getElementById('layer-select').addEventListener('change', (event) => {
+    showOnlyLayer(event.target.value);
+    updateLegend(event.target.value);
 });
